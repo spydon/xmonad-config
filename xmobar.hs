765dbd70ccec986abc3e8ffe710766d436d985df
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2015-02-20 11:27:18 +0100

    Added xmobar to xmonad

---
+++ xmobar.hs
@@ -0,0 +1,38 @@
+Config {
+    font = "xft:Monospace:pixelsize=20",
+    allDesktops = True,
+    commands = [
+        Run MultiCpu       [ "--template" , "Cpu: <total0>% <total1>% <total2>% <total3>%"
+                             , "--Low"      , "50"         -- units: %
+                             , "--High"     , "85"         -- units: %
+                             , "--low"      , "darkgreen"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkred"
+                             ] 100,
+        Run Memory ["-t","Mem: <usedratio>%"] 100,
+        Run Date "%a %b %_d %H:%M" "date" 600,
+        Run DynNetwork [] 100,
+        Run MPD ["-t",
+                  "<artist>: <title> (<album>) <state>", -- <remaining>
+                            "--", "-P", ">>", "-Z", "|", "-S", "><"] 200,
+        Run StdinReader,
+        Run Battery        [ "--template" , "Batt: <acstatus>"
+                             , "--Low"      , "10"        -- units: %
+                             , "--High"     , "80"        -- units: %
+                             , "--low"      , "darkred"
+                             , "--normal"   , "darkorange"
+                             , "--high"     , "darkgreen"
+
+                             , "--" -- battery specific options
+                                       -- discharging status
+                                       , "-o"	, "<left>% (<timeleft>)"
+                                       -- AC "on" status
+                                       , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                       -- charged status
+                                       , "-i"	, "<fc=#006000>Charged</fc>"
+                           ] 100
+    ],
+    sepChar = "%",
+    alignSep = "}{",
+    template = "%StdinReader%  Playing: %mpd% }{ %multicpu% | %dynnetwork% | %memory% | %battery% | <fc=#ee9a00>%date%</fc>"
+}
---
+++ xmonad.hs
@@ -0,0 +1,27 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Gaps
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "~/.cabal/bin/xmobar ~/.xmonad/xmobar.hs"
+  xmonad $ defaultConfig 
+    { terminal = "gnome-terminal"
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+                              { ppOutput = hPutStrLn xmproc
+                              , ppTitle = xmobarColor "green" "" . shorten 50
+                              }
+    }
+      `additionalKeys`
+           [ ((mod1Mask, xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-10'")
+           , ((mod1Mask, xK_Right), spawn "pulseaudio-ctl up")
+           , ((mod1Mask, xK_Left), spawn "pulseaudio-ctl down")
+           , ((mod1Mask, xK_Up), spawn "xbacklight +10")
+           , ((mod1Mask, xK_Down), spawn "xbacklight -10") ]
